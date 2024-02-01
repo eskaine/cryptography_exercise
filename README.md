25b7c2372f45fedc587b846ecba1ffca677d5cd0
Eskaine <eskaine.n4g@gmail.com>
2024-02-01 17:52:09 +0800

    added  keccak256 hashing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# cryptography_exercise
+# Cryptography Challenge : Problem Statement
+
+1. Generate a 32 bytes private key using  any  one of the  Pbkdf2/Scrypt/random submodules 
+2. Calculate the sha256 hash of the message  “Web3 is Awesome”
+3. Compute the public key using secp256k1 submodule from the private key generated in step 1
+4. Use  Keccak256 hash to generate signer’s  Ethereum address from public key generated in step 3
+5. Use  sign function of secp256k1 submodule  and  signer’s private key generated in step 1 to calculate the signature of the message hash generated in step 2 .
+6. Use  verify function of secp256k1 submodule  and  public  key generated in step 3 to verify the signature generated in step 5.
+7. Use recover function of secp256k1 submodule, message hash generated in step 2  ,signature generated in step 5 and  public  key generated in step 3 and recovery bit 1 to recover public key 
+8. Use recovered public key step 7 to generate signer’s Ethereum address
--- src/main.js
+++ src/main.js
@@ -1,24 +1,47 @@
 const { scrypt } = require("ethereum-cryptography/scrypt");
 const { sha256 } = require("ethereum-cryptography/sha256");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
 const { utf8ToBytes } = require("ethereum-cryptography/utils");
 const { bytesToHex } = require("ethereum-cryptography/utils");
 
 function main() {
-  generate32BytesPrivateKey();
-  generate256Hash();
+//   generate32BytesPrivateKey();
+//   generate256Hash();
+//   derivePublicKeyFromPrivateKey();
+  generateEthAddressFromPublicKey();
 }
 
-async function generate32BytesPrivateKey() {
+async function generate32BytesPrivateKey(messageOne = "Web3 is Awesome", messageTwo = "Web3 is Awesome") {
   const privateKey = bytesToHex(
-    await scrypt(utf8ToBytes("Calyptus"), utf8ToBytes("Hello"), 1024, 8, 1, 32)
+    await scrypt(utf8ToBytes(messageOne), utf8ToBytes(messageTwo), 1024, 8, 1, 32)
   );
-
   console.log("32 bytes private key:", privateKey);
+
+  return privateKey;
 }
 
-async function generate256Hash() {
-    const hash = bytesToHex(sha256(utf8ToBytes("Web3 is Awesome")));
+function generate256Hash(message = "Web3 is Awesome") {
+    const hash = bytesToHex(sha256(utf8ToBytes(message)));
     console.log("sha256 hash:", hash);
+
+    return hash;
+}
+
+async function derivePublicKeyFromPrivateKey() {
+    const privateKey = await generate32BytesPrivateKey("web3", "blockchain");
+    const publicKey = secp256k1.getPublicKey(privateKey);
+    console.log("Public Key", publicKey);
+
+    return publicKey;
+}
+
+async function generateEthAddressFromPublicKey() {
+    const publicKey = await derivePublicKeyFromPrivateKey();
+    const hash = bytesToHex(keccak256(publicKey));
+    console.log("Address:", hash);
+
+    return hash;
 }
 
 main();
